c43fd6f9823ec7281604ea5144b5586b003178d5
hengyu <hengyu@outlook.com>
2023-12-10 20:30:51 +0800

    Remove focus guide

--- Sources/Onboarding/Controller/HelpPageViewController.swift
+++ Sources/Onboarding/Controller/HelpPageViewController.swift
@@ -41,7 +41,6 @@ public final class HelpPageViewController: UIPageViewController {
         setupComponents()
         setupConstraints()
         setupPages()
-        setupFocus()
         setupKeyCommands()
         setupGesture()
         registerTraitsUpdate()
@@ -126,11 +125,6 @@ public final class HelpPageViewController: UIPageViewController {
         }
     }
 
-    private func setupFocus() {
-        addFocusGuide(from: cancelButton, to: view, direction: .bottom)
-        addFocusGuide(from: view, to: cancelButton, direction: .top)
-    }
-
     private func setupKeyCommands() {
         let esc = UIKeyCommand(
             input: UIKeyCommand.inputEscape,
@@ -189,41 +183,6 @@ public final class HelpPageViewController: UIPageViewController {
         @unknown default:
             break
         }
-    }
-
-    // https://www.enekoalonso.com/articles/understanding-and-visualizing-uifocusguide-on-tvos
-    @discardableResult
-    private func addFocusGuide(
-        from origin: UIView,
-        to destination: UIView,
-        direction: UIRectEdge
-    ) -> UIFocusGuide {
-        let focusGuide = UIFocusGuide()
-        view.addLayoutGuide(focusGuide)
-        focusGuide.preferredFocusEnvironments = [destination]
-
-        // Configure size to match origin view
-        focusGuide.widthAnchor.constraint(equalTo: origin.widthAnchor).isActive = true
-        focusGuide.heightAnchor.constraint(equalTo: origin.heightAnchor).isActive = true
-
-        switch direction {
-        case .bottom: // swipe down
-            focusGuide.topAnchor.constraint(equalTo: origin.bottomAnchor).isActive = true
-            focusGuide.leftAnchor.constraint(equalTo: origin.leftAnchor).isActive = true
-        case .top: // swipe up
-            focusGuide.bottomAnchor.constraint(equalTo: origin.topAnchor).isActive = true
-            focusGuide.leftAnchor.constraint(equalTo: origin.leftAnchor).isActive = true
-        case .left: // swipe left
-            focusGuide.topAnchor.constraint(equalTo: origin.topAnchor).isActive = true
-            focusGuide.rightAnchor.constraint(equalTo: origin.leftAnchor).isActive = true
-        case .right: // swipe right
-            focusGuide.topAnchor.constraint(equalTo: origin.topAnchor).isActive = true
-            focusGuide.leftAnchor.constraint(equalTo: origin.rightAnchor).isActive = true
-        default:
-            // Not supported :(
-            break
-        }
-        return focusGuide
     }
 
     #if os(tvOS)
